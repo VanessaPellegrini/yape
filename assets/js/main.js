fb2361dd05a35f39c95d6abceced15c2d84e3a26
amalakamala <amalakamala.a@gmail.com>
2017-08-21 13:24:45 -0300

    validar para continuar

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$('#formulario').validate({
+        rules: {
+            inputPhone: {
+                required: true
+            },
+            inputCheckbox: {
+                required: true
+            }
+        }
+    });
+
+    $('#formulario input').on('keyup blur', function () {
+        if ($('#formulario').valid()) {
+        	var elPhone = $('#phone').val();
+
+        	if(elPhone.length != 9){
+				console.log('malo');
+				return false;
+        	}else{
+        		console.log('bueno');
+        	}
+
+            $('button#register').prop('disabled', false);
+        } else {
+            $('button#register').prop('disabled', 'disabled');
+        }
+    });
+});
